f4502411ab5db63c3c363dde87af222b7ec55d07
Ozidear <omerru06@gmail.com>
2026-09-13 02:33:16 +0300

    generated a 1000 random pint pairs

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,19 @@ struct PointPair {
     y1: f64,
 }
 
-fn main() {
-    let mut rng = StdRng::seed_from_u64(1);
-
-    let point_pair = PointPair {
+fn random_point_pair(rng: &mut impl RngExt) -> PointPair {
+    PointPair {
         x0: rng.random_range(-180.0..180.0),
         y0: rng.random_range(-90.0..90.0),
         x1: rng.random_range(-180.0..180.0),
         y1: rng.random_range(-90.0..90.0),
-    };
-    println!("{:?}", point_pair);
+    }
+}
+
+fn main() {
+    let mut rng = StdRng::seed_from_u64(1);
+
+    let point_pairs: [PointPair; 1_000] = std::array::from_fn(|_| random_point_pair(&mut rng));
+
+    println!("{point_pairs:?}");
 }
