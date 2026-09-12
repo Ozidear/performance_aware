b3d178c34d3b95f6f915170e1c02ecbb4eaf35da
Ozidear <omerru06@gmail.com>
2026-09-12 13:48:09 +0300

    Generated a random point pair

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use rand::{RngExt, SeedableRng, rngs::StdRng};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PointPair {
+    x0: f64,
+    y0: f64,
+    x1: f64,
+    y1: f64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = StdRng::seed_from_u64(1);
+
+    let point_pair = PointPair {
+        x0: rng.random_range(-180.0..180.0),
+        y0: rng.random_range(-90.0..90.0),
+        x1: rng.random_range(-180.0..180.0),
+        y1: rng.random_range(-90.0..90.0),
+    };
+    println!("{:?}", point_pair);
 }
