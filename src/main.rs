d373de88e7e0c2eb16780ffc3bb9c679f6bb81cf
Ozidear <omerru06@gmail.com>
2026-09-13 13:26:15 +0300

    now writes the json to a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::Parser;
 use rand::{RngExt, SeedableRng, rngs::StdRng};
 use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 
 #[derive(Parser)]
 struct Args {
@@ -9,6 +14,9 @@ struct Args {
 
     #[arg(long)]
     seed: u64,
+
+    #[arg(long, default_value = "point_pairs.json")]
+    output: PathBuf,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -21,14 +29,14 @@ struct PointPair {
 
 fn random_point_pair(rng: &mut impl RngExt) -> PointPair {
     PointPair {
-        x0: rng.random_range(-180.0..180.0), // is this the best rand algorithm?
+        x0: rng.random_range(-180.0..180.0),
         y0: rng.random_range(-90.0..90.0),
         x1: rng.random_range(-180.0..180.0),
         y1: rng.random_range(-90.0..90.0),
     }
 }
 
-fn main() -> Result<(), serde_json::Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let mut rng = StdRng::seed_from_u64(args.seed);
 
@@ -36,7 +44,10 @@ fn main() -> Result<(), serde_json::Error> {
         .map(|_| random_point_pair(&mut rng))
         .collect();
 
-    println!("{}", serde_json::to_string(&point_pairs)?);
+    let output_file = File::create(args.output)?;
+    let mut writer = BufWriter::new(output_file);
+    serde_json::to_writer(&mut writer, &point_pairs)?;
+    writer.flush()?;
 
     Ok(())
 }
