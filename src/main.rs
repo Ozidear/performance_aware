b3f4c4d69fc6d4ce9bbf41ac7567ad2045bfb010
Ozidear <omerru06@gmail.com>
2026-09-13 18:20:57 +0300

    added haversine avg

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ use std::{
     path::PathBuf,
 };
 
+const EARTH_RADIUS_KM: f64 = 6_372.8;
+
 #[derive(Parser)]
 struct Args {
-    #[arg(long)]
+    #[arg(long, value_parser = clap::value_parser!(u32).range(1..))]
     count: u32,
 
     #[arg(long)]
@@ -27,6 +29,20 @@ struct PointPair {
     y1: f64,
 }
 
+fn haversine(point_pair: &PointPair, earth_radius: f64) -> f64 {
+    let lat1 = point_pair.y0.to_radians();
+    let lat2 = point_pair.y1.to_radians();
+    let delta_lat = lat2 - lat1;
+    let delta_lon = (point_pair.x1 - point_pair.x0).to_radians();
+
+    let sin_delta_lat = (delta_lat / 2.0).sin();
+    let sin_delta_lon = (delta_lon / 2.0).sin();
+    let a = sin_delta_lat * sin_delta_lat + lat1.cos() * lat2.cos() * sin_delta_lon * sin_delta_lon;
+    let c = 2.0 * a.sqrt().asin();
+
+    earth_radius * c
+}
+
 fn random_point_pair(rng: &mut impl RngExt) -> PointPair {
     PointPair {
         x0: rng.random_range(-180.0..180.0),
@@ -38,7 +54,6 @@ fn random_point_pair(rng: &mut impl RngExt) -> PointPair {
 
 //TODO:
 // cluster/uniform
-// haversine avg
 // binary output
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -48,6 +63,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let point_pairs: Vec<PointPair> = (0..args.count)
         .map(|_| random_point_pair(&mut rng))
         .collect();
+
+    let average_haversine = point_pairs
+        .iter()
+        .map(|point_pair| haversine(point_pair, EARTH_RADIUS_KM))
+        .sum::<f64>()
+        / point_pairs.len() as f64;
+    println!("Average Haversine distance: {average_haversine}");
 
     let output_file = File::create(args.output)?;
     let mut writer = BufWriter::new(output_file);
