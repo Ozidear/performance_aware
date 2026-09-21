17837c7b54726f1e6710e6b36627a70171e2fe14
Ozidear <omerru06@gmail.com>
2026-09-21 18:40:02 +0300

    Squashed commit of the following:
    commit 6aeaafaf0977585ca184d347be8bab4063f2f815
    Author: Ozidear <omerru06@gmail.com>
    Date:   Mon Sep 21 18:35:49 2026 +0300
    
        moved generation under /bin
    
    commit 9b6f42bc69c91ef43cc9884b2d4686088a522658
    Author: Ozidear <omerru06@gmail.com>
    Date:   Mon Sep 21 17:59:24 2026 +0300
    
        Added a comment
    
    commit 914189217d3b1196b2ad3731665254f2baacebc2
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 19:56:17 2026 +0300
    
        added an answer file
    
    commit f1bbe85f62c25931833fdac84f47a1ca4395839e
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 19:45:18 2026 +0300
    
        Now iterates once and avoids holding the vec in memory
    
    commit b3f4c4d69fc6d4ce9bbf41ac7567ad2045bfb010
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 18:20:57 2026 +0300
    
        added haversine avg
    
    commit eb1e234792e970decee364a03be3cd3069ea29c0
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 13:38:58 2026 +0300
    
        added a todo list
    
    commit d373de88e7e0c2eb16780ffc3bb9c679f6bb81cf
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 13:26:15 2026 +0300
    
        now writes the json to a file
    
    commit bc01915f55832c89e0fce8d2091064a2523cea33
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 12:44:54 2026 +0300
    
        Now printing json
    
    commit 548ed8cd6fb1de2b4a4b8a2bb08327cda5026788
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 03:35:43 2026 +0300
    
        Added clap in order to make it a proper cli
    
    commit f4502411ab5db63c3c363dde87af222b7ec55d07
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sun Sep 13 02:33:16 2026 +0300
    
        generated a 1000 random pint pairs
    
    commit b3d178c34d3b95f6f915170e1c02ecbb4eaf35da
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sat Sep 12 13:48:09 2026 +0300
    
        Generated a random point pair
    
    commit f8ded65ba05be0494ee341bdded4bbdc4345e79c
    Author: Ozidear <omerru06@gmail.com>
    Date:   Sat Sep 12 13:12:33 2026 +0300
    
        added rustfmt on save
    
    commit 7e830aaa90267734a0071dffe84208df0b463907
    Author: Ozidear <omerru06@gmail.com>
    Date:   Fri Sep 11 22:04:17 2026 +0300
    
        added serde depends

---
+++ src/bin/generate.rs
@@ -0,0 +1,94 @@
+use clap::Parser;
+use rand::{RngExt, SeedableRng, rngs::StdRng};
+use serde::ser::{SerializeSeq, Serializer};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
+
+const EARTH_RADIUS_KM: f64 = 6_372.8;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    count: usize,
+
+    #[arg(long)]
+    seed: u64,
+
+    #[arg(long, default_value = "point_pairs.json")]
+    json_output: PathBuf,
+
+    #[arg(long, default_value = "haversine_answers.f64")]
+    answer_output: PathBuf,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PointPair {
+    x0: f64,
+    y0: f64,
+    x1: f64,
+    y1: f64,
+}
+
+fn haversine(point_pair: &PointPair, earth_radius: f64) -> f64 {
+    let lat1 = point_pair.y0.to_radians();
+    let lat2 = point_pair.y1.to_radians();
+    let delta_lat = lat2 - lat1;
+    let delta_lon = (point_pair.x1 - point_pair.x0).to_radians();
+
+    let sin_delta_lat = (delta_lat / 2.0).sin();
+    let sin_delta_lon = (delta_lon / 2.0).sin();
+    let a = sin_delta_lat * sin_delta_lat + lat1.cos() * lat2.cos() * sin_delta_lon * sin_delta_lon;
+    let c = 2.0 * a.sqrt().asin();
+
+    earth_radius * c
+}
+
+fn random_point_pair(rng: &mut impl RngExt) -> PointPair {
+    PointPair {
+        x0: rng.random_range(-180.0..180.0),
+        y0: rng.random_range(-90.0..90.0),
+        x1: rng.random_range(-180.0..180.0),
+        y1: rng.random_range(-90.0..90.0),
+    }
+}
+
+//TODO:
+// cluster/uniform?
+// could improve this a ton by declaring functions that will act as context managers
+// taking closures and returning results
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let mut rng = StdRng::seed_from_u64(args.seed);
+
+    let mut json_writer = BufWriter::new(File::create(args.json_output)?);
+    let mut serializer = serde_json::Serializer::new(&mut json_writer);
+    let mut point_pairs = serializer.serialize_seq(Some(args.count))?;
+
+    let mut answer_writer = BufWriter::new(File::create(args.answer_output)?);
+
+    let mut haversine_sum = 0.0;
+
+    for _ in 0..args.count {
+        let point_pair = random_point_pair(&mut rng);
+        point_pairs.serialize_element(&point_pair)?;
+
+        let haversine_distance = haversine(&point_pair, EARTH_RADIUS_KM);
+        answer_writer.write_all(&haversine_distance.to_le_bytes())?;
+        haversine_sum += haversine_distance;
+    }
+    point_pairs.end()?;
+    json_writer.flush()?;
+
+    answer_writer.write_all(&haversine_sum.to_le_bytes())?;
+    answer_writer.flush()?;
+
+    let average_haversine = haversine_sum / args.count as f64;
+    println!("Average Haversine distance: {average_haversine}");
+
+    Ok(())
+}
